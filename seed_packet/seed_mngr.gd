d02e428d49c891faec1143784e329a584ca67ac9
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-23 22:11:54 +0200

    Added seed packet ui

--- seed_packet/seed_mngr.gd
+++ seed_packet/seed_mngr.gd
@@ -1,7 +1,38 @@
-extends Node
+extends Control
 class_name SeedMngr
 
+@onready var seed_container: VBoxContainer = %seed_container
+
 var seeds: Dictionary[PlantResource, int] = {}
+
+func update_seed_display() :
+  for i in seed_container.get_children():
+    i.queue_free()
+  
+  for i in seeds:
+    if seeds[i] <= 0: continue
+  
+    var horizontal: HBoxContainer = HBoxContainer.new()
+    
+    var control: Control = Control.new()
+    
+    var sprite: TextureRect = TextureRect.new()
+    sprite.texture = i.packet_sprite
+    sprite.custom_maximum_size = Vector2(32, 32)
+    
+    control.add_child(sprite)
+    control.custom_minimum_size = Vector2(32, 32)
+    
+    horizontal.add_child(control)
+    
+    var text: RichTextLabel = RichTextLabel.new()
+    text.text = "x%d" % seeds[i]
+    text.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+    text.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+    
+    horizontal.add_child(text)
+    
+    seed_container.add_child(horizontal)
 
 func add_seed(plant: PlantResource) -> void:
   if plant in seeds:
@@ -9,10 +40,10 @@ func add_seed(plant: PlantResource) -> void:
   else:
     seeds[plant] = 1
   
-  print(seeds)
+  update_seed_display()
 
 func remove_seed(plant: PlantResource) -> void:
   if plant in seeds and seeds[plant] > 0:
     seeds[plant] -= 1
   
-  print(seeds)
+  update_seed_display()
