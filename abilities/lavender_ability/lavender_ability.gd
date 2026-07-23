a1fc7dbce40b984c3ce81ff676abde51f679a3db
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-23 17:16:28 +0200

    Added particles to lavender tp

--- abilities/lavender_ability/lavender_ability.gd
+++ abilities/lavender_ability/lavender_ability.gd
@@ -1,10 +1,22 @@
 extends Ability
 
-var max_tp_range: float = 200.0
+const LAVENDER_PARTICLE = preload("uid://dfyd1os38ckwx")
+
+var max_tp_range: float = 300.0
 
 func apply_effect(player: Player, target: Vector2) -> void:
   var diff: Vector2 = target - player.global_position
   
-  player.global_position += (diff.normalized() * max_tp_range) if diff.length_squared() > max_tp_range * max_tp_range else diff
+  var blink_by: Vector2 = (diff.normalized() * max_tp_range) if diff.length_squared() > max_tp_range * max_tp_range else diff
+  
+  var end_pos: Vector2 = player.global_position + blink_by
+  
+  for i in [0.0, 0.25, 0.5, 0.75]:
+    var part: GPUParticles2D = LAVENDER_PARTICLE.instantiate()
+    part.global_position = player.global_position + (end_pos - player.global_position) * i
+  
+    Qol.add_to_tree(part)
+  
+  player.global_position = end_pos
   
   set_cooldown(4.5 / plant_stage)
