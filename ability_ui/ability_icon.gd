6f14d8addb22a45408e800e46f497a8418633d22
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-23 14:47:40 +0200

    Added ccountdown to ability icon

--- ability_ui/ability_icon.gd
+++ ability_ui/ability_icon.gd
@@ -2,6 +2,7 @@ extends Control
 class_name AbilityIcon
 
 @onready var icon: TextureRect = %icon
+@onready var countdown: RichTextLabel = %countdown
 
 @export var ability: Ability
 
@@ -13,6 +14,9 @@ func _process(_delta: float) -> void:
   var progress: float = 1.0
   
   if ability.cooldown:
+    countdown.text = "%.1f" % ability.cooldown.time_left
     progress = (ability.cooldown.wait_time - ability.cooldown.time_left) / ability.cooldown.wait_time
 
   (icon.material as ShaderMaterial).set_shader_parameter("progress", progress)
+
+  countdown.visible = ability.cooldown and ability.cooldown.time_left > 0
