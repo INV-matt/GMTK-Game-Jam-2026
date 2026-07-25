e4f07bc2630eaabc193e1eba3add5a932b4b7a56
INV-matt <matteomarini888@gmail.com>
2026-07-25 15:19:30 +0200

    Plants total health ui

--- player_ui/health_ui.gd
+++ player_ui/health_ui.gd
@@ -4,20 +4,20 @@ extends Control
 @onready var bar_plant: ProgressBar = %plant_bar
 
 @onready var pl: Player = Qol.player
-var connected = false
+var pl_connected = false
 
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
-  if !pl:
-    pl = Qol.player
-  if pl:
-    if !connected:
-      pl.hp_comp.hurt.connect(update_player_bar)
-      pl.hp_comp.healed.connect(update_player_bar)
-      #pl.hp_comp.died.connect(update_player_bar)
+  if !pl: pl = Qol.player
+  if pl && !pl_connected:
+    pl.hp_comp.hurt.connect(update_player_bar)
+    pl.hp_comp.healed.connect(update_player_bar)
+    #pl.hp_comp.died.connect(update_player_bar)
 
-      update_player_bar(0)
-      connected = true
+    update_player_bar(0)
+    pl_connected = true
+
+  update_plant_bar()
 
 
 @warning_ignore("unused_parameter")
@@ -26,4 +26,13 @@ func update_player_bar(amount: float) -> void:
   bar_player.value = clamp(pl.hp_comp.hp, 0, Qol.player.hp_comp.max_hp)
 
 func update_plant_bar() -> void:
-  bar_plant.value = clamp(randi() % 100, 0, 100)
+  var total = 0
+  var curr = 0
+  for plant: Node in get_tree().get_nodes_in_group("plants"):
+    var hp_comp: HpComp = Qol.find_hp_comp(plant)
+    if hp_comp:
+      total += hp_comp.max_hp
+      curr += hp_comp.hp
+      print("%s / %s" % [curr, total])
+  bar_plant.value = clamp(curr, 0, total)
+  bar_plant.max_value = total
