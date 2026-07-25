25e55cf8d7e7ab5f74b8ce58caf6f1cd9e4ae714
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-25 10:31:59 +0200

    Implemented wave countdown

--- player_ui/enemy_ui.gd
+++ player_ui/enemy_ui.gd
@@ -7,11 +7,28 @@ const KILLS_TEXT: String = "Enemy kills: %s/%s"
 @onready var txt_waves: Label = %wave_counter
 @onready var txt_enemy: Label = %enemy_counter
 @onready var txt_kills: Label = %kills_needed
+@onready var wave_countdown: Label = %wave_countdown
 
 func _ready() -> void:
   Signals.started_wave.connect(update_waves)
   Signals.enemy_change.connect(update_enemy)
   Signals.kills_update.connect(update_kills)
+  Signals.wave_timer_update.connect(update_timer)
+
+func update_timer(time: float) -> void:
+  var minutes: int = int(time / 60)
+  
+  if minutes > 0:
+    var seconds: float = time - minutes * 60
+    
+    if seconds >= 10:
+      wave_countdown.text = "%s:%.1f" % [minutes, time - minutes * 60]
+      return
+      
+    wave_countdown.text = "%s:0%.1f" % [minutes, time - minutes * 60]
+    return
+    
+  wave_countdown.text = "%.1f" % time
 
 func update_waves(wave: int) -> void:
   txt_waves.text = WAVE_TEXT % wave
