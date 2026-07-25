3092b828acc33ded291b186dec52f85c3258b83d
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-25 19:16:55 +0200

    Added final stats

--- lose_screen/lose_screen.gd
+++ lose_screen/lose_screen.gd
@@ -8,6 +8,16 @@ var damage_dealt: int = 0
 var damage_recieved: int = 0
 var rewards_taken: int = 0
 var upgrades_taken: int = 0
+var waves_survived: int = 0
+
+var total_score: int = 0:
+  get():
+    return  enemies_killed * 20 + \
+            damage_dealt * 5 + \
+            damage_recieved * -10 + \
+            rewards_taken * -5 + \
+            upgrades_taken * -1 + \
+            waves_survived * 20
 
 func _ready() -> void:
   visible = false
@@ -20,6 +30,7 @@ func _ready() -> void:
   Signals.damage_recieved.connect(func(amt: int): damage_recieved += amt)
   Signals.reward_selected.connect(func(_x: Reward): rewards_taken += 1)
   Signals.killed_enough_enemies.connect(func(): upgrades_taken += 1)
+  Signals.started_wave.connect(func(num: int): waves_survived = num - 1)
 
 func show_lose_screen():
   Qol.pause_game()
@@ -31,6 +42,8 @@ func add_stat(stat_name: String):
   r.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
   r.fit_content = true
   r.text = "%s: %s" % [stat_name.capitalize(), self[stat_name.replace(" ", "_")]]
+  r.size_flags_vertical = Control.SIZE_EXPAND_FILL
+  r.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 
   stat_holder.add_child(r)
 
@@ -40,3 +53,5 @@ func display_stats():
   add_stat("damage recieved")
   add_stat("rewards taken")
   add_stat("upgrades taken")
+  add_stat("waves survived")
+  add_stat("total score")
