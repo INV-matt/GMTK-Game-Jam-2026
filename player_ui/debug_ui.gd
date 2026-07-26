44e8c5db922e22c805fbf62640fac8ae12a204ad
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-26 11:03:40 +0200

    Implemented simple 'tutorial glyphs'

--- player_ui/debug_ui.gd
+++ player_ui/debug_ui.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var txt_fps: Label = %fps
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
   if Globals.DEBUG:
     visible = true
     txt_fps.text = "%d fps" % Engine.get_frames_per_second()
---
+++ tutorial/tutorial_glyph.gd
@@ -0,0 +1,31 @@
+extends Node2D
+class_name TutorialGlyph
+
+@onready var sprite: Sprite2D = %sprite
+@onready var text: RichTextLabel = %text
+@onready var anim: AnimationPlayer = %anim
+
+@export var action_name: String = ""
+@export var action_icon: Texture2D
+
+@export var action_desc: String = ""
+@export var action_group: Array[TutorialGlyph] = []
+
+var completed: bool = false
+
+func _ready() -> void:
+  sprite.texture = action_icon
+  text.text = action_desc
+  action_group.append(self)
+
+func _process(_delta: float) -> void:
+  if !completed:
+    if Input.is_action_pressed(action_name):
+      completed = true
+      anim.play("fade_away")
+  elif len(action_group.filter(func(x):
+    if is_instance_valid(x):
+      return !x.completed
+    return false
+  )) == 0:
+    anim.play("fade_away_fr")
