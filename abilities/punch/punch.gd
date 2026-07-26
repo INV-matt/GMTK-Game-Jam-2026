cbc2c6809b84dfb64c2d2705fa6283611e01696c
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-26 17:44:35 +0200

    Implemented punch vfx

--- abilities/punch/punch.gd
+++ abilities/punch/punch.gd
@@ -1,5 +1,7 @@
 extends Ability
 
+const PUNCH_ABILTY_VFX = preload("uid://djbmr4x712ofn")
+
 func apply_effect(player: Player, target: Vector2) -> void:
   var diff: Vector2 = target - player.global_position
   
@@ -9,6 +11,13 @@ func apply_effect(player: Player, target: Vector2) -> void:
   (hitbox.custom_shape as CircleShape2D).radius = 50
   hitbox.global_position = player.global_position + diff.normalized() * 75
   hitbox.lifetime = .1
+  
+  var spr: Sprite2D = Sprite2D.new()
+  spr.texture = PUNCH_ABILTY_VFX
+  spr.scale = Vector2(.1, .1)
+  spr.rotation = diff.angle() + PI
+  
+  hitbox.add_child(spr)
   
   Qol.add_to_tree(hitbox)
   
