42f3b7236e171a3ef5b74ffa8173ba1b673fdb59
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-07-26 12:01:17 +0200

    Implemented placeholder textures + Ability tutorial

--- tutorial/tutorial_glyph.gd
+++ tutorial/tutorial_glyph.gd
@@ -1,3 +1,5 @@
+@tool
+
 extends Node2D
 class_name TutorialGlyph
 
@@ -7,6 +9,7 @@ class_name TutorialGlyph
 
 @export var action_name: String = ""
 @export var action_icon: Texture2D
+@export var sprite_scale: Vector2 = Vector2(.25, .25)
 
 @export var action_desc: String = ""
 @export var action_group: Array[TutorialGlyph] = []
@@ -16,9 +19,16 @@ var completed: bool = false
 func _ready() -> void:
   sprite.texture = action_icon
   text.text = action_desc
+  sprite.scale = sprite_scale
   action_group.append(self)
 
 func _process(_delta: float) -> void:
+  if Engine.is_editor_hint():
+    sprite.scale = sprite_scale
+    sprite.texture = action_icon
+    text.text = action_desc
+    return
+  
   if !completed:
     if Input.is_action_pressed(action_name):
       completed = true
